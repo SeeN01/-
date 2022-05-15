bf7d8889b716a0376759969f74b2c2af5c13a1c4
SeeN01 <105580884+SeeN01@users.noreply.github.com>
2022-05-15 12:42:06 +0300

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+
+using namespace std;
+int main()
+{	srand(time(0));
+	setlocale(LC_ALL, "RUS");
+	int a, b, t = 0, r, c = 0;
+	cout << "Введите границы"<<endl;
+	cin >> a >> b;
+	r = rand() % b + a;
+	cout << "Угадывайте число"<<endl;
+	while (c != r) {
+		t++;
+		c = 0;
+		cin >> c;
+		if (c < r)cout << "Больше"<< endl;
+		if (c > r)cout << "Меньше"<< endl;
+		if (c == r)cout << "Вы угадали вам понадобилось попыток - " << t;
+	
+	}
+	
+}
+
+
